5a25e13f4fbeac594a12bfdeb73cf9df3d9fe097
samwerry <league@WTS-IM-17.attlocal.net>
2019-03-05 19:55:34 -0800

    fibo,pi
    fufnfufnf fufnf fnfuf f

--- src/algorithms/GuessingPi.java
+++ src/algorithms/GuessingPi.java
@@ -8,10 +8,12 @@ import javax.swing.JOptionPane;
 
 public class GuessingPi {
 	// 1. Make a main method and make sure your program can run
+public static void main(String[] args) {
+	
 
 		// 2. Make a String variable to hold the value of Pi.
 		//    Get the first few digits from http://www.piday.org/million/.
-
+String p = "3.1415926535897932384626433";
 		// 3. Print out the first 3 digits of Pi to the console.
 		//    The first value is "pi.charAt(0)", the second is "pi.charAt(1)", etc.
 		//    Run your program to see how this works.
@@ -22,18 +24,29 @@ public class GuessingPi {
 			// 4. Create a for loop that will step through each digit of pi (steps 5-8)
 			//    NOTE: The number of digits in your string will be
 			//       yourStringVariable.length()
+for(int i = 0;i< p.length();i++) {
+	System.out.println(p.charAt(i));
+	
 
 				// 5. Ask the user for the NEXT digit of pi.
-
+String a = JOptionPane.showInputDialog(null, "What is the next digit of pi");
 				// 6. Compare the user's input to the next digit of your pi variable 
 				//    (look at step 3 for a clue).
 				//    HINT: use charAt(0) to get 1st char of user input String 
 
+if (a.equals  (p.charAt(2)+"")) {
+	System.out.println("correct");
+}
 					// 7. If they are correct, print out "correct".
 
 					// 8. If they are not, print out "incorrect" and tell them
 					//    to start over. Use 'break;' to break out of the loop.
+else {
+	System.out.println("incorrect");
+	break;
+}
 
 }
 
-
+}
+}
---
+++ src/algorithms/fibnicco.java
@@ -0,0 +1,29 @@
+package algorithms;
+
+public class fibnicco {
+public static void main(String[] args) {
+	
+
+	
+int z = 0;
+int b = 1;
+int temp = 0;
+for(int i = 0;i<25;i++) {
+temp = z;
+z=b;
+b=temp+z;
+
+
+System.out.println(z);
+}
+
+
+
+
+
+
+
+
+}
+
+}
